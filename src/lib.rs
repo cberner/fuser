0f802e210a1777ae96055d8a5f5af87261ae4fb7
jcarl <jcarl@locusrobotics.com>
2024-03-27 10:17:02 -0400

    Rename that last fd to fh

--- src/lib.rs
+++ src/lib.rs
@@ -331,10 +331,10 @@ pub trait Filesystem {
     }
 
     /// Get file attributes.
-    fn getattr(&mut self, _req: &Request<'_>, ino: u64, fd: Option<u64>, reply: ReplyAttr) {
+    fn getattr(&mut self, _req: &Request<'_>, ino: u64, fh: Option<u64>, reply: ReplyAttr) {
         warn!(
-            "[Not Implemented] getattr(ino: {:#x?}, fd: {:#x?})",
-            ino, fd
+            "[Not Implemented] getattr(ino: {:#x?}, fh: {:#x?})",
+            ino, fh
         );
         reply.error(ENOSYS);
     }
