8340b1290f35587eb9579e1980639bc6a660e3fc
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-05-29 11:02:25 +0200

    Add job to run tests on macOS

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -5,7 +5,7 @@ use std::time::Duration;
 use tempfile::TempDir;
 
 #[test]
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 fn unmount_no_send() {
     // Rc to make this !Send
     struct NoSendFS(Rc<()>);
