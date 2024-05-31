4139d4f2d26129ec888d838334ec21a275908e6b
Firelight Flagboy <firelight.flagboy@gmail.com>
2024-05-31 09:51:34 +0200

    Add debug log for integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -8,16 +8,22 @@ use tempfile::TempDir;
 #[cfg(any(target_os = "linux", target_os = "macos"))]
 fn unmount_no_send() {
     // Rc to make this !Send
+    env_logger::init();
     struct NoSendFS(Rc<()>);
 
     impl Filesystem for NoSendFS {}
 
     let tmpdir: TempDir = tempfile::tempdir().unwrap();
     let mut session = Session::new(NoSendFS(Rc::new(())), tmpdir.path(), &[]).unwrap();
+    log::debug!("Session created");
     let mut unmounter = session.unmount_callable();
     thread::spawn(move || {
         thread::sleep(Duration::from_secs(1));
+        log::debug!("unmounting");
         unmounter.unmount().unwrap();
+        log::debug!("unmounted");
     });
+    log::debug!("running session");
     session.run().unwrap();
+    log::debug!("session finished")
 }
