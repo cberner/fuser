5960b7bebd7bbe414bb21f3e479c4886ce993ed6
Nicolas Papp <nicolaspapp@gmail.com>
2025-05-29 14:23:34 -0300

    Change flag to whenever libfuse is not enabled

--- src/ll/fuse_abi.rs
+++ src/ll/fuse_abi.rs
@@ -597,9 +597,9 @@ pub struct fuse_mkdir_in {
 #[derive(Debug, FromBytes, KnownLayout, Immutable)]
 pub struct fuse_rename_in {
     pub newdir: u64,
-    #[cfg(all(feature = "macfuse-4-compat", not(target_os = "macos")))]
+    #[cfg(all(feature = "macfuse-4-compat", not(feature = "libfuse")))]
     pub flags: u32,
-    #[cfg(all(feature = "macfuse-4-compat", not(target_os = "macos")))]
+    #[cfg(all(feature = "macfuse-4-compat", not(feature = "libfuse")))]
     pub padding: u32,
 }
 
