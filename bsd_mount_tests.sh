c03d8be663db08fc01a07b2de9f99176e31ec0e3
Christopher Berner <me@cberner.com>
2025-09-14 19:05:29 -0700

    Make code build on macos and BSD without libfuse flag

--- bsd_mount_tests.sh
+++ bsd_mount_tests.sh
@@ -16,8 +16,8 @@ RED="\e[31m"
 
 function run_test {
   DIR=$(mktemp -d)
-  cargo build --example hello $1 > /dev/null 2>&1
-  cargo run --example hello $1 -- $DIR $3 &
+  cargo build --example hello > /dev/null 2>&1
+  cargo run --example hello -- $DIR $2 &
   FUSE_PID=$!
   sleep 2
 
@@ -26,9 +26,9 @@ function run_test {
   mount | grep hello || exit 1
 
   if [[ $(cat ${DIR}/hello.txt) = "Hello World!" ]]; then
-      echo -e "$GREEN OK $2 $3 $NC"
+      echo -e "$GREEN OK $1 $2 $NC"
   else
-      echo -e "$RED FAILED $2 $3 $NC"
+      echo -e "$RED FAILED $1 $2 $NC"
       export TEST_EXIT_STATUS=1
       exit 1
   fi
@@ -37,9 +37,9 @@ function run_test {
   wait $FUSE_PID
 }
 
-run_test --features=libfuse 'with libfuse'
+run_test 'with libfuse'
 
 # TODO: auto unmount doesn't seem to be supported on FreeBSD
-# run_test --features=libfuse 'with libfuse' --auto_unmount
+# run_test 'with libfuse' --auto_unmount
 
 export TEST_EXIT_STATUS=0
