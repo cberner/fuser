0996925c3b9857f057f8c2223ba1c0917a84e1c0
Davide Rizzo <sorcio@gmail.com>
2025-10-17 11:16:42 +0200

    clippy build.rs

--- build.rs
+++ build.rs
@@ -10,33 +10,31 @@ fn main() {
 
     if target_os == "linux" && cfg!(not(feature = "libfuse")) {
         println!("cargo:rustc-cfg=fuser_mount_impl=\"pure-rust\"");
+    } else if target_os == "macos" {
+        pkg_config::Config::new()
+            .atleast_version("2.6.0")
+            .probe("fuse") // for macFUSE 4.x
+            .map_err(|e| eprintln!("{e}"))
+            .unwrap();
+        println!("cargo:rustc-cfg=fuser_mount_impl=\"libfuse2\"");
+        println!("cargo:rustc-cfg=feature=\"macfuse-4-compat\"");
     } else {
-        if target_os == "macos" {
+        // First try to link with libfuse3
+        if pkg_config::Config::new()
+            .atleast_version("3.0.0")
+            .probe("fuse3")
+            .map_err(|e| eprintln!("{e}"))
+            .is_ok()
+        {
+            println!("cargo:rustc-cfg=fuser_mount_impl=\"libfuse3\"");
+        } else {
+            // Fallback to libfuse
             pkg_config::Config::new()
                 .atleast_version("2.6.0")
-                .probe("fuse") // for macFUSE 4.x
+                .probe("fuse")
                 .map_err(|e| eprintln!("{e}"))
                 .unwrap();
             println!("cargo:rustc-cfg=fuser_mount_impl=\"libfuse2\"");
-            println!("cargo:rustc-cfg=feature=\"macfuse-4-compat\"");
-        } else {
-            // First try to link with libfuse3
-            if pkg_config::Config::new()
-                .atleast_version("3.0.0")
-                .probe("fuse3")
-                .map_err(|e| eprintln!("{e}"))
-                .is_ok()
-            {
-                println!("cargo:rustc-cfg=fuser_mount_impl=\"libfuse3\"");
-            } else {
-                // Fallback to libfuse
-                pkg_config::Config::new()
-                    .atleast_version("2.6.0")
-                    .probe("fuse")
-                    .map_err(|e| eprintln!("{e}"))
-                    .unwrap();
-                println!("cargo:rustc-cfg=fuser_mount_impl=\"libfuse2\"");
-            }
         }
     }
 }
