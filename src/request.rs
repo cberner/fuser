de99416afbc2ab289d007b6b2e8ab45b921ae3d2
Stepan Koltsov <stepan.koltsov@gmail.com>
2026-01-19 01:39:23 +0000

    Remove now not needed feature check

--- src/request.rs
+++ src/request.rs
@@ -92,7 +92,6 @@ impl<'a> Request<'a> {
                     | ll::Operation::FSyncDir(_)
                     | ll::Operation::Release(_)
                     | ll::Operation::ReleaseDir(_) => {}
-                    #[cfg(feature = "abi-7-21")]
                     ll::Operation::ReadDirPlus(_) => {}
                     _ => {
                         return Err(Errno::EACCES);
