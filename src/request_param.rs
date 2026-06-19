0a9de0175f83c337d22fb553f2282fad982d9ad5
Khanh Tran <khanhtranngoccva@gmail.com>
2026-06-19 11:42:45 +0700

    feat: add cloning for request structure for 'static capturing in custom error structures

--- src/request_param.rs
+++ src/request_param.rs
@@ -11,6 +11,23 @@ pub struct Request {
     header: fuse_in_header,
 }
 
+impl Clone for Request {
+    fn clone(&self) -> Self {
+        Request {
+            header: fuse_in_header {
+                len: self.header.len,
+                opcode: self.header.opcode,
+                unique: self.header.unique,
+                nodeid: self.header.nodeid,
+                uid: self.header.uid,
+                gid: self.header.gid,
+                pid: self.header.pid,
+                padding: self.header.padding,
+            },
+        }
+    }
+}
+
 impl Request {
     #[ref_cast_custom]
     pub(crate) fn ref_cast(header: &fuse_in_header) -> &Request;
